83ac6d7f31a4b545364028d25b688a519b6ff244
yth345 <48090907+yth345@users.noreply.github.com>
2022-12-31 14:07:39 +0800

    Add files via upload

---
+++ code/leaf/leaf_gen.py
@@ -0,0 +1,29 @@
+def leaf(a,b,c,d):
+    f = (a^b) + (b^c) - (c^d)
+    return f
+def toHex(x):
+    if x >= 0:
+        return x
+    else:
+        return 2**32+x
+
+if __name__ == '__main__':
+    # Modify your test pattern here
+    # initial: a = 6, b = 9, c = 14, d = 4
+    a = 3
+    b = 9
+    c = 5
+    d = 17
+
+    with open('leaf_data_2.txt', 'w') as f_data:
+        f_data.write('{:0>8x}\n'.format(toHex(a)))
+        f_data.write('{:0>8x}\n'.format(toHex(b)))
+        f_data.write('{:0>8x}\n'.format(toHex(c)))
+        f_data.write('{:0>8x}\n'.format(toHex(d)))
+
+    with open('leaf_data_ans_2.txt', 'w') as f_ans:
+        f_ans.write('{:0>8x}\n'.format(toHex(a)))
+        f_ans.write('{:0>8x}\n'.format(toHex(b)))
+        f_ans.write('{:0>8x}\n'.format(toHex(c)))
+        f_ans.write('{:0>8x}\n'.format(toHex(d)))
+        f_ans.write('{:0>8x}\n'.format(toHex(leaf(a,b,c,d))))
